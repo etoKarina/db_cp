29a5df6585f30fa9eb7a0948525bd533737e6f33
Karina <doctorwhosamgreen@gmail.com>
2021-11-29 23:50:24 +0300

    zabila

---
+++ real_interface.py
@@ -0,0 +1,47 @@
+from tkinter import Tk, ttk
+import tkinter as tk
+from business_logic import Restoraunt
+
+from tab_a import Example as TabA
+from tab_b import Example as TabB
+from tab_c import Example as TabC
+from tab_d import Example as TabD
+
+
+class MainWindow(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.parent = parent
+        self.parent.title('Gourmets')
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.parent['padx'] = 10
+        self.parent['pady'] = 10
+
+        self.notebook = ttk.Notebook(self, width=1000, height=700)
+
+        a_tab = TabA(self.notebook)
+        b_tab = TabB(self.notebook)
+        c_tab = TabC(self.notebook)
+        d_tab = TabD(self.notebook)
+
+
+        self.notebook.add(a_tab, text="Посетителям")
+        self.notebook.add(b_tab, text="Поставщикам")
+        self.notebook.add(c_tab, text="Персоналу")
+        self.notebook.add(d_tab, text="Документация")
+
+        self.notebook.pack()
+
+        self.pack()
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Gourmets')
+    ex = MainWindow(root)
+    root.geometry("800x600")
+    root.mainloop()
