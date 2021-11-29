2363ee7cd64df0ed717be85523d5ac3a52b30c26
Karina <doctorwhosamgreen@gmail.com>
2021-11-29 23:42:35 +0300

    bruh

---
+++ tab_d.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+from business_logic import Restoraunt
+
+
+class Example(tk.Frame):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.parent = parent
+        self.init_ui()
+        self.restoraunt = Restoraunt()
+
+    def init_ui(self):
+        self.button = tk.Button(self, text='Список блюд', command=self.on_click)
+        self.button.pack()
+
+        self.pack()
+
+        self.button2 = tk.Button(self, text='Список ингредиентов', command=self.on_click3)
+        self.button2.pack()
+
+        self.pack()
+
+        self.somelabel = tk.Label(self, text='here comes something')
+        self.somelabel.pack()
+
+    def on_click(self):
+        result = self.restoraunt.db.get_all_dish()
+        self.somelabel['text'] = result
+
+    def on_click2(self):
+        result = self.restoraunt.db.get_all_products()
+        self.somelabel['text'] = result
+
+    def on_click3(self):
+        products = self.restoraunt.db.get_all_products()
+        height = len(products)
+        width = 5
+
+        for i in range(height):  # Rows
+            for j in range(width):  # Columns
+                b = tk.Entry(self, text="")
+                b.grid(row=i, column=j)
+
